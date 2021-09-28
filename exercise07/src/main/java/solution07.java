dd3bc5b70d54c0096e3f66ea501d96f47342c525
JesseLekwa <jesse.lekwa@knights.ucf.edu>
2021-09-28 13:03:33 -0400

    commit

--- exercise07/src/main/java/solution07.java
+++ exercise07/src/main/java/solution07.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class solution07 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 7
+ *  Copyright 2021 jesse lekwa
+ */
+
+import java.util.Scanner;
+
+public class solution07 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the length of the room in feet?");
+        double length=sc.nextDouble();
+        System.out.println("What is the width of the room in feet?");
+        double width=sc.nextDouble();
+        System.out.println("You entered dimension of "+length+" by "+width+" feet");
+
+
+        double area=length*width;
+        final double conversionFactor=area*0.09290304;
+
+
+        System.out.println("The area is");
+        System.out.println(area+" square feet");
+        System.out.println(conversionFactor+" square meters");
+    }
 }
